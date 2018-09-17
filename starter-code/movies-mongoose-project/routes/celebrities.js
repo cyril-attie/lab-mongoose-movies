e08079f236bf8d6334fa4c08c75828d61bec272d
cyril.attie <attiecyril@gmail.com>
2018-09-17 17:57:12 +0200

    Iteration #4: Adding New Celebrities

--- starter-code/movies-mongoose-project/routes/celebrities.js
+++ starter-code/movies-mongoose-project/routes/celebrities.js
@@ -1,44 +1,54 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const Celebrity = require('../models/celebrity')
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/mongoose-movies-project', {useNewUrlParser: true})
-  
+mongoose.connect('mongodb://localhost/mongoose-movies-project', { useNewUrlParser: true })
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  Celebrity.find().then(found=>console.log(found))
+  Celebrity.find().then(found => console.log(found))
   res.render('index');
 
 });
 
 router.get('/celebrities', (req, res, next) => {
-  Celebrity.find({}) 
-  .then((celebrities) => {
+  Celebrity.find({})
+    .then((celebrities) => {
       console.log(celebrities)
       res.render("celebrities/index", { celebrities });
     })
     .catch(error => {
       console.log(error)
     })
- });
+});
 
 router.get('/celebrity/:id', (req, res, next) => {
   let celebrityId = req.params.id;
-  if (!/^[0-9a-fA-F]{24}$/.test(celebrityId)) { 
+  if (!/^[0-9a-fA-F]{24}$/.test(celebrityId)) {
     return res.status(404).render('not-found');
   }
-  Celebrity.findOne({'_id': celebrityId})
+  Celebrity.findOne({ '_id': celebrityId })
     .then(celebrity => {
       if (!celebrity) {
-          return res.status(404).render('not-found');
+        return res.status(404).render('not-found');
       }
       res.render("celebrities/show", { celebrity })
     })
     .catch(next)
 });
 
+router.get('/celebrities/new', (req, res, next) => {
+  res.render("celebrities/new")
+})
 
+router.post("/celebrities", (req, res, next) => {
+  let { name, occupation, catchPhrase } = req.body
+  const newCelebrity = new Celebrity({ name, occupation, catchPhrase })
+  newCelebrity.save()
+  .then((celebrity)=> res.redirect("/celebrities"))
+  .catch((err)=>res.redirect("celebrities/new"))
+})
 
 
 module.exports = router;
